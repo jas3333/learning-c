8d9e0cd7e60decd8375a5b046d06b87ac0c11d84
jas3333 <accounts1@protonmail.com>
2022-06-09 05:52:49 -0700

    CLI app to configure picom

---
+++ projects/picom_configure/header/main.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <ncurses.h>
+#include <string.h>
+
+
+
+// Prototypes
+
+int get_value(char search_text[]);
+void get_bool(char search_text[], char *pointer);
+void update_value(int new_value, char search_string[], char updated_string[]);
+void update_value_float(float new_value, char search_string[], char updated_string[]);
+void update_string(char update_string[], char search_string[]);
+float get_float(char search_text[]);
+void shadows(int row, int column);
+int length(char *string);
+
---
+++ projects/picom_configure/scratch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int int_values[5] = {1, 2, 3, 4};
+
+    printf("%d", int_values[0]);
+
+    return 0;
+}
